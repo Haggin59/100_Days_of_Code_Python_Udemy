ed5a38206d5dc43ab4aea5d59677f45b739a7d19
Haggin59 <hagginsajeev@gmail.com>
2022-11-19 00:17:36 +0530

    19.1

--- Day 19/main.py
+++ Day 19/main.py
@@ -1,4 +1,7 @@
+#turtle race
+
 from turtle import Turtle,Screen
+import random
 
 #hp kittila
 
@@ -6,7 +9,7 @@ scr = Screen()
 scr.setup(width=500, height=400)
 
 user_input = scr.textinput("Input", "Enter your bet (color):")
-colors = ['red', 'orange', 'green', 'yellow', 'blue', 'purple']
+colors = ['red', 'orange', 'green', 'yellow', 'blue']
 
 turtles = []
 x = -230
@@ -21,7 +24,27 @@ for i in range(5):
     turtles[i].goto(x,y)
     y -= 50
 
+run = True
 
+while run:
+
+    for i in range(5):
+
+        turtles[i].forward(random.randint(10,30))
+        
+        p = turtles[i].position()
+        if p[0] >= 250:
+            run = False
+            break
+
+print("user_input",user_input)
+print("Winner:", i)
+
+if user_input == str(i):
+    print("You Win")
+
+else:
+    print("You loose")
  
 
 
